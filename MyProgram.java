d25363d2ccadd517b00034afb6d4edbb19d1c81e
v-khimanshu05 <verma96himanshu@gmail.com>
2023-10-14 00:16:22 +0530

    Concatenate

--- MyProgram.java
+++ MyProgram.java
@@ -1,11 +1,17 @@
 import java.time.LocalDateTime;
 
 public class MyProgram{
+	static void myfun(String a,String b)
+	{
+		System.out.println("Concatenate : "+(a+b));
+	}
 	public static void main(String[] args)
 	{
  		System.out.println("WELCOME Himanshu!!");
 		System.out.println("And now i am pushing code");
 		System.out.println(LocalDateTime.now());
 		System.out.println("ADD"+(5+1));
+		myfun("him","verma");
+
 	}
 }
