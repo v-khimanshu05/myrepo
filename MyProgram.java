848395236a69ed8b49bbd37eeb580b4e852dfb21
v-khimanshu05 <verma96himanshu@gmail.com>
2023-10-14 00:02:29 +0530

    Adding date calculate

--- MyProgram.java
+++ MyProgram.java
@@ -1,7 +1,10 @@
+import java.time.LocalDateTime;
+
 public class MyProgram{
 	public static void main(String[] args)
 	{
  		System.out.println("WELCOME Himanshu!!");
 		System.out.println("And now i am pushing code");
+		System.out.println(LocalDateTime.now());
 	}
 }
